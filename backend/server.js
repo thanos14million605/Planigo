06c14b85d24f684a4c9dd85373170c34d3b05c10
Ebrima Gajaga <gajaga.dev@gmail.com>
2026-03-20 19:42:58 +0530

    Modified server.js

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ pool
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontendts/dist")));
 
-  app.get("*", (req, res) => {
+  app.get((req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
   });
 }
