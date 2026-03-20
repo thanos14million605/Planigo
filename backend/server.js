3bec1dbf7e61dcf33345cfc53b4c349330b9b09f
Ebrima Gajaga <gajaga.dev@gmail.com>
2026-03-20 19:52:06 +0530

    Updated path resolution in server.js

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,12 @@ pool
   .catch((err) => console.log("Error in DB connection", err));
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontendts/dist")));
+  const frontendPath = path.resolve(__dirname, "../frontend/dist");
 
-  app.get((req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontendts", "dist", "index.html"));
+  app.use(express.static(frontendPath));
+
+  app.use((_, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
 
