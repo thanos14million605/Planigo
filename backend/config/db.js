af23296c7ac7cf56fa9d1b156cc21b07a1989494
Ebrima Gajaga <gajaga.dev@gmail.com>
2026-03-20 20:18:44 +0530

    Modified config/db.js, utils/jwtHelper.js, and server.js environment variable resolution

--- backend/config/db.js
+++ backend/config/db.js
@@ -8,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 dotenv.config({
-  path: path.resolve(__dirname, "./../config.env"),
+  path: path.resolve(__dirname, "./../../config.env"),
 });
 
 const isProduction = process.env.NODE_ENV === "production";
